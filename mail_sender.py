fd4c788228689bacf203fbb5c70b776f2806ebe3
IyadElwy <iyadelwy1@gmail.com>
2021-07-21 01:31:56 +0200

    Added feature to get the changed site as an html attachment in the email but without loading resources. The method to highlight changes in the html does not work yet.

--- mail_sender.py
+++ mail_sender.py
@@ -1,21 +1,60 @@
 import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 class Mail:
 
-    def __init__(self, mail, password):
+    def __init__(self, mail: str, password: str):
+        """
+        Initializing the Mailing system object with the needed data.
+        :param mail: your mail
+        :type mail: str
+        :param password: your mail password
+        :type password: str
+        """
         self._port = 465
         self._smtp_server_domain_name = "smtp.gmail.com"
         self._sender_mail = mail
         self._password = password
+        self._html_msg = ""
 
-    def send(self, email, subject, content):
+    def _convert_to_sendable_html(self) -> str:
+        msg_root = MIMEMultipart('related')
+        msg_root['Subject'] = "Website Notifier"
+
+        msg_alternative = MIMEMultipart('alternative')
+        msg_root.attach(msg_alternative)
+
+        msg_text = MIMEText(self._html_msg, 'html')
+        msg_alternative.attach(msg_text)
+
+        return msg_root.as_string()
+
+    def send(self, email: str, subject: str, content: str, html_msg_to_send: str = ""):
+        """
+        The function to call when sending a mail
+        :param email: the mail you wish to send to
+        :type email: str
+        :param subject: the subject of the mail
+        :type subject: str
+        :param content: The content of the mail
+        :type content: str
+        :param html_msg_to_send: the html message to send
+        :type html_msg_to_send: str
+        :return: None
+        :rtype: None
+        """
         ssl_context = ssl.create_default_context()
         service = smtplib.SMTP_SSL(self._smtp_server_domain_name, self._port,
                                    context=ssl_context)
         service.login(self._sender_mail, self._password)
 
-        result = service.sendmail(self._sender_mail, email,
-                                  f"Subject: {subject}\n{content}")
-
+        if html_msg_to_send:
+            self._html_msg = html_msg_to_send
+            service.sendmail(self._sender_mail, email,
+                             self._convert_to_sendable_html())
+        else:
+            service.sendmail(self._sender_mail, email,
+                             f"Subject: {subject}\n{content}")
         service.quit()
--- main.py
+++ main.py
@@ -27,11 +27,13 @@ class Website:
         self._password = password
         self._mail_content = message_to_send
 
-    def start_monitoring(self, monitoring_intervals_in_seconds=60):
+    def start_monitoring(self, monitoring_intervals_in_seconds: int = 60, send_html: bool = False):
         """
         The method that starts the monitoring process.
         :param monitoring_intervals_in_seconds: The intervals of time for monitoring the website (seconds).
         :type monitoring_intervals_in_seconds: int
+        :param send_html: if the mail should contain the html changes
+        :type send_html: bool
         :return: None
         :rtype: None
         """
@@ -52,7 +54,11 @@ class Website:
                                      f"{changes[1]}\n\n" \
                                      f"LINES IN HTML:\n" \
                                      f"{self._find_html_line_to_mark(changes[1], new_file_contents)}"
-                mail.send(self._email_address, "Website Notifier", content_to_be_sent)
+                if send_html:
+                    mail.send(self._email_address, "Website Notifier", content_to_be_sent,
+                              self._create_full_html_site())
+                else:
+                    mail.send(self._email_address, "Website Notifier", content_to_be_sent)
                 break
 
             time.sleep(monitoring_intervals_in_seconds)
@@ -75,11 +81,15 @@ class Website:
         with open(file_name, "r", encoding="utf8") as html_file:
             html_file_as_string = html_file.readlines()
 
-            # TODO: remove debugging code here
-            # with open("C:/Users/iyade/PycharmProjects/TestStuff/testWebsite.html",
-            #           "r", encoding="utf8") as html_file:
-
-            html_file_as_string = html_file.readlines()
+        # TODO: remove debugging code here
+        # if is_new_file:
+        #     with open("C:/Users/iyade/PycharmProjects/TestStuff/testWebsite.html",
+        #               "r", encoding="utf8") as html_file:
+        #         html_file_as_string = html_file.readlines()
+        # else:
+        #     with open("C:/Users/iyade/PycharmProjects/TestStuff/testWebsite2.html",
+        #               "r", encoding="utf8") as html_file:
+        #         html_file_as_string = html_file.readlines()
 
         return html_file_as_string
 
@@ -128,14 +138,57 @@ class Website:
             list_with_changed_lines_indexes.append(file_to_search_in.index(line) + 1)
         return list_with_changed_lines_indexes
 
+    def _create_full_html_site(self):
+        """
+        Helper method
+        :return: The html page as a single string object
+        :rtype: str
+        """
+        html_page = self._convert_to_string(True)
+        message = f"""
+        <div class="header">
+        <h1>{self._mail_content}</h1>
+        </div>
+        """
+
+        # TODO: Fix
+        # new_file_contents = self._convert_to_string(True)
+        # current_file_contents = self._convert_to_string(False)
+        # changes = self._detect_specific_change(current_file_contents,
+        #                                        new_file_contents, False)
+        # html_page = self._create_box_over_changes(self._find_html_line_to_mark(changes[0], current_file_contents),
+        #                                           html_page)
+
+        html_page.insert(0, message)
+        return "\n".join(html_page)
+
+    @staticmethod
+    def _create_box_over_changes(list_with_indexes: list[int], html_page: list[str]) -> list[str]:
+        # TODO: Fix
+        """
+        Helper method to circle the changes in the html
+        :param list_with_indexes: the list with the index of the html lines that have changed
+        :type list_with_indexes: list[int]
+        :param html_page: the full html page as a list
+        :type html_page: list[str]
+        :return: the html page as a list but with the changed lines surrounded by a square
+        :rtype: list[str]
+        """
+        html_to_return = html_page
+        for index in list_with_indexes:
+            temp_line = html_to_return[index]
+            line_with_square = f"""{temp_line[0:2]}style="border:3px; border-style:solid; border-color:#FF0000; 
+            padding: 1em;"{temp_line[2:]}"""
+            html_to_return[index] = line_with_square
+
+        return html_to_return
+
 
 if __name__ == '__main__':
     file = Website(link="https://www.w3schools.com/python/ref_string_rindex.asp",
-                   email_address="TEST@gmail.com", password="PASSWORD",
-                   message_to_send="Your site changed!")
-    file.start_monitoring(5)
+                   email_address="test@gmail.com", password="Password",
+                   message_to_send="Change on Website detected!")
+    file.start_monitoring(2, True)
 
-# TODO: use the html of the original site to send an html view of the website via email
-# TODO: but with changes highlighted using the indexes of the changed lines
 # TODO: Add input option to quit with the input validation module and use the time
 # TODO: out module to check for when the program should continue instead of the sleep function
